8652f89ba81d1ecfcd30a589f4da331353879ef4
Dmitry Gavrilov <gavrstyle@gmail.com>
2022-02-01 15:22:03 +0500

    all done but will need refactoring

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import selectTestimonials from './selectTestimonials.js'
+import videoModal from './videoModal.js'
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    selectTestimonials()
+    videoModal()
+})
